6a5bf1078f4d193c576869d13a3e40ce0fd6d025
Martín Trejo Chávez <chilli.coder@gmail.com>
2017-10-21 12:08:52 -0500

    Formateo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 const routes: Routes = [
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      { path: 'dashboard',    component: DashboardComponent },
-      { path: 'detail/:id',   component: HeroDetailComponent },
-      { path: 'heroes',       component: HeroesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',    component: DashboardComponent },
+  { path: 'detail/:id',   component: HeroDetailComponent },
+  { path: 'heroes',       component: HeroesComponent }
 ];
 
 @NgModule({
